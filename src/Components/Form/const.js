1bc2083e06432de928b5e5ca4e44eb7bcc1fa090
Eeshu009 <eeshu@algogenix.com>
2024-03-11 18:38:22 +0530

    ---updateCode

--- src/Components/Form/const.js
+++ src/Components/Form/const.js
@@ -2,4 +2,4 @@ const queryParams = new URLSearchParams(window.location.search);
  export const mId = queryParams.get('merchantId');
  export const fId = queryParams.get('formId');
 
-console.log("mid",mId, fId);
+console.log("mid",mId, fId); 
